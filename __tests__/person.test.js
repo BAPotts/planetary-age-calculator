a280d02d87d61c963d067b64c49a07fba7699542
Beverly Potts <pottsbeverly@gmail.com>
2020-06-26 18:45:20 -0400

    Fail getPlanets() test

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -2,50 +2,54 @@ import { Person } from '../src/person.js';
 
 describe('Person', () => {
 
-  test('Should take and store an age', () =>
+  // test('Should take and store an age', () =>
+  // {
+  //   let beverly = new Person(32);
+  //   expect(beverly.earthAge).toEqual(32);
+  // })
+  // test('Should take earthAge and calculate age in Mercury years', ()=>
+  // {
+  //   let beverly = new Person(32);
+  //   beverly.ageCalc();
+  //   expect(beverly.planetAges[0]).toEqual(133);
+  // })
+  // test('Should take an earthAge and calculate age in Venus years', ()=>
+  // {
+  //   let beverly = new Person(32);
+  //   beverly.ageCalc();
+  //   expect(beverly.planetAges[1]).toEqual(52);
+  // })
+  // test('Should take an earthAge and calculate age in Mars years', ()=>
+  // {
+  //   let beverly = new Person(32);
+  //   beverly.ageCalc();
+  //   expect(beverly.planetAges[2]).toEqual(17);
+  // })
+  // test('Should take an earthAge and calculate age in Jupiter years', ()=>
+  // {
+  //   let beverly = new Person(32);
+  //   beverly.ageCalc();
+  //   expect(beverly.planetAges[3]).toEqual(3);
+  // })
+  // test('Should take in the Earth life expectancy of user', ()=>
+  // {
+  //   let beverly = new Person(32, 79);
+  //   expect(beverly.earthExpectancy).toEqual(79);
+  // })
+  // test('Should convert earthExpectancy to life expectancy of other planets and calculate years left to live on each', ()=>
+  // {
+  //   let beverly = new Person(32,79);
+  //   beverly.ageCalc();
+  //   expect(beverly.yearsLeft).toEqual([196, 75, 25, 4]);
+  // })
+  // test('Should give years lived over life expectancy on planet if age is greater than life expectancy', ()=>
+  // {
+  //   let george = new Person(82, 79);
+  //   george.ageCalc();
+  //   expect(george.yearsOver).toEqual([13, 5, 2, 0]);
+  // })
+  test('Should return object literal with planet names and years as compared to earth years', ()=>
   {
-    let beverly = new Person(32);
-    expect(beverly.earthAge).toEqual(32);
-  })
-  test('Should take earthAge and calculate age in Mercury years', ()=>
-  {
-    let beverly = new Person(32);
-    beverly.ageCalc();
-    expect(beverly.planetAges[0]).toEqual(133);
-  })
-  test('Should take an earthAge and calculate age in Venus years', ()=>
-  {
-    let beverly = new Person(32);
-    beverly.ageCalc();
-    expect(beverly.planetAges[1]).toEqual(52);
-  })
-  test('Should take an earthAge and calculate age in Mars years', ()=>
-  {
-    let beverly = new Person(32);
-    beverly.ageCalc();
-    expect(beverly.planetAges[2]).toEqual(17);
-  })
-  test('Should take an earthAge and calculate age in Jupiter years', ()=>
-  {
-    let beverly = new Person(32);
-    beverly.ageCalc();
-    expect(beverly.planetAges[3]).toEqual(3);
-  })
-  test('Should take in the Earth life expectancy of user', ()=>
-  {
-    let beverly = new Person(32, 79);
-    expect(beverly.earthExpectancy).toEqual(79);
-  })
-  test('Should convert earthExpectancy to life expectancy of other planets and calculate years left to live on each', ()=>
-  {
-    let beverly = new Person(32,79);
-    beverly.ageCalc();
-    expect(beverly.yearsLeft).toEqual([196, 75, 25, 4]);
-  })
-  test('Should give years lived over life expectancy on planet if age is greater than life expectancy', ()=>
-  {
-    let george = new Person(82, 79);
-    george.ageCalc();
-    expect(george.yearsOver).toEqual([13, 5, 2, 0]);
+    expect(getPlanets()).toEqual(planets);
   })
 })
--- src/person.js
+++ src/person.js
@@ -7,21 +7,23 @@ export class Person {
     this.yearsOver=[];
   }
 
-  ageCalc() {
-    const planets = {
-        name: ["Mercury", "Venus", "Mars", "Jupiter"],
-      yearLength: [.24, .62, 1.88, 11.86]
-    }
-    for (let i = 0; i<planets.yearLength.length; i++){
-      let planetAge = Math.round(this.earthAge/planets.yearLength[i]);
-      let planetExpectancy = Math.round(this.earthExpectancy/planets.yearLength[i]);
-      let planetYearsLeft = Math.round(planetExpectancy-planetAge);
-      this.planetAges.push(planetAge);
-      if(this.earthAge<=this.earthExpectancy){
-        this.yearsLeft.push(planetYearsLeft);
-      } else{
-        this.yearsOver.push(Math.abs(planetYearsLeft));
-      }
-    }
-  }
+  getPlanets(){
+  }  
+  // ageCalc() {
+  //   const planets = {
+  //     name: ["Mercury", "Venus", "Mars", "Jupiter"],
+  //     yearLength: [.24, .62, 1.88, 11.86]
+  //   }
+  //   for (let i = 0; i<planets.yearLength.length; i++){
+  //     let planetAge = Math.round(this.earthAge/planets.yearLength[i]);
+  //     let planetExpectancy = Math.round(this.earthExpectancy/planets.yearLength[i]);
+  //     let planetYearsLeft = Math.round(planetExpectancy-planetAge);
+  //     this.planetAges.push(planetAge);
+  //     if(this.earthAge<=this.earthExpectancy){
+  //       this.yearsLeft.push(planetYearsLeft);
+  //     } else{
+  //       this.yearsOver.push(Math.abs(planetYearsLeft));
+  //     }
+  //   }
+ // }
 }
